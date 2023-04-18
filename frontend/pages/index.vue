45db918a804e3e924e1d0eb8d100bad56e4966ba
Xaninho <alexandre1999neves@gmail.com>
2023-04-18 16:51:46 +0100

    🎨 Chatr.js Experiment in Home Dashboard

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,18 +1,65 @@
 <template>
-  <div class="text-center">
-    
-    <div>
-      <MyIcon scale="50" />
+  <main>
+    <div class="card">
+        <Chart type="bar" :data="chartData" :options="chartOptions" />
     </div>
-    
-    <h2 class="text-2xl pb-6"> HandShake CRM in early development! </h2>
-
-  </div>
+    <div class="card card-container">
+        <Chart type="pie" :data="chartData" :options="chartOptions" class="chart-container" />
+        <Chart type="line" :data="chartData" :options="chartOptions" class="chart-container" />
+    </div>
+  </main>
+  
 </template>
 
-<script setup lang='ts'>
+<script lang='ts'>
 
-/// <reference types="vite-svg-loader" />
-import MyIcon from '@/assets/NuxtIllustration.svg?component'
+
+export default defineComponent({
+  name: "HomeDashboard",
+  data() {
+    return {
+       chartData: {
+          labels: ['Q1', 'Q2', 'Q3', 'Q4'],
+          datasets: [
+              {
+                  label: 'Sales',
+                  data: [540, 325, 702, 620],
+                  backgroundColor: ['rgba(255, 159, 64, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(153, 102, 255, 0.2)'],
+                  borderColor: ['rgb(255, 159, 64)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102, 255)'],
+                  borderWidth: 1
+              }
+          ]
+        },
+        chartOptions: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    }
+   
+  }
+});
+
+
 
 </script>
+
+<style scoped>
+  .card-container {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+
+.chart-container {
+  flex: 1;
+  margin-right: 1rem;
+  margin-bottom: 1rem;
+}
+
+.chart-container:last-child {
+  margin-right: 0;
+}
+</style>
--- frontend/plugins/primeVuePlugin.ts
+++ frontend/plugins/primeVuePlugin.ts
@@ -1,7 +1,9 @@
 import PrimeVue from 'primevue/config';
 import Button from 'primevue/button';
+import Chart from 'primevue/chart';
 
 export default defineNuxtPlugin(nuxtApp => {
     nuxtApp.vueApp.use(PrimeVue, {ripple: true});
     nuxtApp.vueApp.component('Button', Button);
+    nuxtApp.vueApp.component('Chart', Chart);
 })
